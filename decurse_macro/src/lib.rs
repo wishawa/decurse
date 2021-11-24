bb3cf4bb636e7f2794bbb6c70bc7ad05b880068a
Wisha Wa <wisha.wa@yandex.com>
2021-11-24 05:50:50 +0000

    bring over macro improvement

--- decurse_macro/src/lib.rs
+++ decurse_macro/src/lib.rs
@@ -8,27 +8,33 @@ use syn::{
     token::Comma,
     Error, Expr, ExprCall, FnArg, ItemFn, Pat, Stmt, Token, Visibility,
 };
-
-struct Parsed(ItemFn);
+struct Parsed(TokenStream);
 
 impl Parse for Parsed {
     fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
         let f: ItemFn = input.parse()?;
         if let Some(a) = &f.sig.asyncness {
-            return Err(Error::new(a.span, "Async function not supported."));
+            return Err(Error::new(a.span, "Decurse: async function not supported."));
         }
-        Ok(Self(f))
+        let generated = generate(f)?;
+        Ok(Self(generated))
     }
 }
 
 struct Folder {
     ident: Ident,
+    closure_nested: usize,
+    async_nested: usize,
+    errors: Vec<Error>,
 }
 
 impl Folder {
     fn new(ident: &Ident) -> Self {
         Self {
             ident: ident.clone(),
+            closure_nested: 0,
+            async_nested: 0,
+            errors: Vec::new(),
         }
     }
 }
@@ -39,52 +45,82 @@ fn generate_call(call: &ExprCall) -> Expr {
 
 impl Fold for Folder {
     fn fold_expr(&mut self, node: Expr) -> Expr {
-        if let Expr::Call(c) = &node {
-            if let Expr::Path(p) = &*c.func {
-                if p.path.segments.last().unwrap().ident == self.ident {
-                    return generate_call(c);
+        match &node {
+            Expr::Call(c) => {
+                if let Expr::Path(p) = &*c.func {
+                    let ident = &p.path.segments.last().unwrap().ident;
+                    if ident == &self.ident {
+                        if self.closure_nested > 0 {
+                            self.errors.push(Error::new(
+                                ident.span(),
+                                "Decurse: recursive call inside closure not supported.",
+                            ));
+                        }
+                        if self.async_nested > 0 {
+                            self.errors.push(Error::new(
+                                ident.span(),
+                                "Decurse: recursive call inside async block not supported.",
+                            ));
+                        }
+                        return generate_call(c);
+                    }
                 }
+                fold_expr(self, node)
             }
+            Expr::Closure(_) => {
+                self.closure_nested += 1;
+                let r = fold_expr(self, node);
+                self.closure_nested -= 1;
+                r
+            }
+            Expr::Async(_) => {
+                self.async_nested += 1;
+                let r = fold_expr(self, node);
+                self.async_nested -= 1;
+                r
+            }
+            _ => fold_expr(self, node),
         }
-        fold_expr(self, node)
     }
 }
 
-impl Parsed {
-    fn generate(self) -> Result<TokenStream, Error> {
-        let mut new = self.0;
-        let name = new.sig.ident.clone();
-        let sig = new.sig.clone();
-        let args = new.sig.inputs.clone();
-        let arg_pats: Punctuated<Pat, Comma> = args
-            .iter()
-            .filter_map(|a| match a {
-                FnArg::Typed(t) => Some(*t.pat.clone()),
-                _ => None,
-            })
-            .collect();
-
-        new.vis = Visibility::Inherited;
-        new.sig.asyncness = Some(Token!(async)(Span::call_site()));
-
-        // Modifying body
-        let mut folder = Folder::new(&name);
-        let stmts: Vec<Stmt> = new
-            .block
-            .stmts
-            .into_iter()
-            .map(|stmt| folder.fold_stmt(stmt))
-            .collect();
-        new.block.stmts = stmts;
-
-        // Create wrapper
-        Ok(quote! {
-            #sig {
-                #new
-                ::decurse::execute( #name(#arg_pats) )
-            }
+fn generate(mut new: ItemFn) -> Result<TokenStream, Error> {
+    // Extracting infos
+    let name = new.sig.ident.clone();
+    let sig = new.sig.clone();
+    let args = new.sig.inputs.clone();
+    let arg_names: Punctuated<Pat, Comma> = args
+        .iter()
+        .filter_map(|a| match a {
+            FnArg::Typed(t) => Some(*t.pat.clone()),
+            _ => None,
         })
+        .collect();
+
+    // Modifying signature
+    new.vis = Visibility::Inherited;
+    new.sig.asyncness = Some(Token!(async)(Span::call_site()));
+
+    // Modifying body
+    let mut folder = Folder::new(&name);
+    let stmts: Vec<Stmt> = new
+        .block
+        .stmts
+        .into_iter()
+        .map(|stmt| folder.fold_stmt(stmt))
+        .collect();
+    new.block.stmts = stmts;
+    if let Some(e) = folder.errors.into_iter().next() {
+        return Err(e);
     }
+
+    // Create wrapper
+    Ok(quote! {
+        #sig {
+            #new
+            ::decurse::execute(#name(#arg_names))
+        }
+    })
 }
 
 #[proc_macro_attribute]
@@ -92,7 +128,6 @@ pub fn decurse(
     _attr: proc_macro::TokenStream,
     item: proc_macro::TokenStream,
 ) -> proc_macro::TokenStream {
-    let func = parse_macro_input!(item as Parsed);
-    let generated = func.generate().unwrap();
-    generated.into()
+    let parsed = parse_macro_input!(item as Parsed);
+    parsed.0.into()
 }
